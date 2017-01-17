edc5ed97ca19685fedd32b11a97d04e1d2ba2224
zackattacknyu <zackattacknyu@gmail.com>
2017-01-17 00:21:46 -0800

    d

--- init.m
+++ init.m
@@ -6,10 +6,30 @@ curSampleFolder = uigetdir;
 imtool3D(dcmArrayHU)
 %%
 
+w     = 2;       % bilateral filter half-width
+sigma = [2 0.1]; % bilateral filter standard deviations
+
+% Apply bilateral filter to each image.
+
+dcmArrayFiltered = zeros(size(dcmArrayHU));
+
+minFactor = min(dcmArrayHU(:));
+divFactor = max(dcmArrayHU(:)) - minFactor;
+
+dcmArrayHUScaled = (dcmArrayHU-minFactor)./(divFactor);
+curSlice = zeros(size(dcmArrayHU,1),size(dcmArrayHU,2),1);
+%for i = 1:size(dcmArrayHU,3)
+for i=13:40
+    fprintf('Slice %d\n',i);
+    curSlice(:,:,1)=dcmArrayHUScaled(:,:,i);
+    dcmArrayFiltered(:,:,i)=bfilter2(curSlice,w,sigma);
+end
+
+dcmArrayFilteredRedone = (dcmArrayFiltered.*divFactor) + minFactor;
+
+%%
 dcmArrayGoodPixels = double(dcmArrayHU>-1200).*double(dcmArrayHU<-800);
 imtool3D(dcmArrayGoodPixels)
 %%
 
-
-%%
 graythresh(dcmArrayHU)
