2061f1dab9f3df2847f5afab3e1cfe856bcfa372
zackattacknyu <zackattacknyu@gmail.com>
2017-01-19 15:29:42 -0800

    progress!!

--- initSegmentation.m
+++ initSegmentation.m
@@ -6,6 +6,22 @@ curSampleFolder = uigetdir;
 imtool3D(dcmArrayHU)
 %%
 
+tissueRegion = find(dcmArrayHU>=500);
+[rV,cV,zV] = ind2sub(size(dcmArrayHU),tissueRegion);
+minR = min(rV); maxR = max(rV);
+minC = min(cV); maxC = max(cV);
+minZ = min(zV); maxZ = max(zV);
+
+dcmArraySeg = dcmArrayHU(minR:maxR,minC:maxC,minZ:maxZ);
+%%imtool3D(dcmArraySeg);
+
+region1 = find(dcmArraySeg>-1200 & dcmArraySeg<-700);
+binBlock = zeros(size(dcmArraySeg));
+binBlock(region1)=1;
+
+blockData = bwconncomp(binBlock);
+
+%{
 outRegion = find(dcmArrayHU<=-1200);
 region1 = find(dcmArrayHU>-1200 & dcmArrayHU<-700);
 region2 = find(dcmArrayHU>=-700);
@@ -14,20 +30,30 @@ volBlock = zeros(size(dcmArrayHU));
 volBlock(outRegion)=1;
 volBlock(region1)=2;
 volBlock(region2)=3;
-%%
+
+binBlock = zeros(size(dcmArrayHU));
+binBlock(region1)=1;
+%}
+
+
+numBlocks = size(blockData.PixelIdxList,2);
+sizes = zeros(1,numBlocks);
+for i = 1:numBlocks
+   sizes(i) = size(blockData.PixelIdxList{i},1); 
+end
+[numPixels,largestBlocks]=sort(sizes,'descend');
 
 %used to obtain bounding box for bones
 %   since lungs are surrounded by ribs, this is good choice
-tissueRegion = find(dcmArrayHU>=500); 
-[rV,cV,zV] = ind2sub(size(dcmArrayHU),tissueRegion);
+%tissueRegion = find(dcmArrayHU>=500); 
+%tissueRegion = find(dcmArrayHU>=100);
+tissueRegion = blockData.PixelIdxList{largestBlocks(1)};
+[rV,cV,zV] = ind2sub(size(dcmArraySeg),tissueRegion);
 minR = min(rV); maxR = max(rV);
 minC = min(cV); maxC = max(cV);
 minZ = min(zV); maxZ = max(zV);
 
-dcmArraySeg = dcmArrayHU(minR:maxR,minC:maxC,minZ:maxZ);
+dcmArraySeg2 = dcmArraySeg(minR:maxR,minC:maxC,minZ:maxZ);
 
-imtool3D(dcmArraySeg);
+imtool3D(dcmArraySeg2);
 
-%%
-
-graythresh(dcmArrayHU)
