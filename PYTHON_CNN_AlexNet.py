d96675c895bc7d56ac10e0f0ec5dcd6874c9ebf3
zackattacknyu <zackattacknyu@gmail.com>
2017-01-31 02:53:46 -0800

    f

---
+++ PYTHON_CNN_AlexNet.py
@@ -0,0 +1,163 @@
+'''Trains a simple convnet on the MNIST dataset.
+
+Gets to 99.25% test accuracy after 12 epochs
+(there is still a lot of margin for parameter tuning).
+16 seconds per epoch on a GRID K520 GPU.
+'''
+
+from __future__ import print_function
+import numpy as np
+import os
+np.random.seed(1337)  # for reproducibility
+
+from keras.datasets import mnist
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Activation, Flatten
+from keras.layers import Convolution2D, MaxPooling2D
+from keras.layers import Convolution3D, MaxPooling3D
+from keras.utils import np_utils
+from keras import backend as K
+
+import scipy.io as sio
+from scipy.misc import imread, imresize, imsave
+import time
+import datetime
+import csv
+from convnetskeras.convnets import preprocess_image_batch, convnet
+from sklearn.cross_validation import train_test_split
+from sklearn import random_projection
+from sklearn import tree
+from sklearn import svm
+from sklearn.ensemble import RandomForestClassifier
+
+
+batch_size = 20
+nb_classes = 2
+nb_epoch = 10
+
+# input image dimensions
+img_rows = 256
+img_cols = 256
+img_sli = 100
+
+# number of convolutional filters to use
+nb_filters = 10
+# size of pooling area for max pooling
+pool_size = (5,5,5)
+# convolution kernel size
+kernel_size = (4,4,4)
+
+matFiles = []
+trainTestIDs = []
+trainTestLabels = []
+validationIDs = []
+
+with open('stage1_labels.csv') as csvfile:
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+        trainTestIDs.append(row['id'])
+        trainTestLabels.append(row['cancer'])
+
+with open('stage1_sample_submission.csv') as csvfile:
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+        validationIDs.append(row['id'])
+
+#TODO: CHANGE WHEN DONE TESTING
+trainingRatio = 0.90
+numTrainTestAll = len(trainTestIDs)
+#numTrainTestAll = 100
+
+numTrain = int(np.floor(trainingRatio*numTrainTestAll))
+numTest = numTrainTestAll-numTrain
+numValid = len(validationIDs)
+
+randInds = np.random.permutation(numTrainTestAll)
+indsTrain = randInds[0:numTrain]
+indsTest = randInds[numTrain:numTrainTestAll]
+
+if K.image_dim_ordering() == 'th':
+    input_shape = (1, img_rows, img_cols,img_sli)
+else:
+    input_shape = (img_rows, img_cols,img_sli, 1)
+
+def getVolData(patID):
+    patFile = "/home/zdestefa/data/segFilesResizedAll/resizedSegDCM_" + patID + ".mat"
+    curMATcontent = sio.loadmat(patFile)
+    volData = curMATcontent["resizedDCM"]
+    return volData
+
+def dataGenerator(patIDnumbers, patLabels, indsUse):
+    while 1:
+        for ind in range(len(indsUse)):
+            patID = patIDnumbers[indsUse[ind]]
+            XCur = getVolData(patID)
+            if K.image_dim_ordering() == 'th':
+                XCur = XCur.reshape(1, 1, img_rows, img_cols, img_sli)
+            else:
+                XCur = XCur.reshape(1, img_rows, img_cols, img_sli, 1)
+            YCur = int(patLabels[indsUse[ind]])
+            YUse = np_utils.to_categorical(YCur, nb_classes)
+            #print("Ind:" + str(ind))
+            yield (XCur.astype('float32'),YUse)
+
+def validDataGenerator():
+    while 1:
+        for ind in range(len(validationIDs)):
+            patID = validationIDs[ind]
+            XCur = getVolData(patID)
+            if K.image_dim_ordering() == 'th':
+                XCur = XCur.reshape(1, 1, img_rows, img_cols, img_sli)
+            else:
+                XCur = XCur.reshape(1, img_rows, img_cols, img_sli, 1)
+            #print("ValidInd:" + str(ind))
+            yield (XCur.astype('float32'))
+
+def validDataGenerator2D():
+    while 1:
+        for ind in range(len(validationIDs)):
+            patID = validationIDs[ind]
+            XCur = getVolData(patID)
+            for slice in range(img_sli):
+                currentXslice = XCur[:,:,slice]
+                currentInput = np.zeros((3,227,227))
+                currentInput[0,:,:] = imresize(currentXslice,(227,227))
+                currentInput = currentInput.reshape(1,3,227,227)
+                # print("ValidInd:" + str(ind))
+                yield (currentInput.astype('float32'))
+
+
+#Here is code from a 3D CNN example on the following blog:
+#   http://learnandshare645.blogspot.in/2016/06/3d-cnn-in-keras-action-recognition.html
+#
+#Good initial CNN tutorial:
+#   http://machinelearningmastery.com/handwritten-digit-recognition-using-convolutional-neural-networks-python-keras/
+
+
+alexmodel = convnet('alexnet')
+alexmodel.compile(loss='categorical_crossentropy', optimizer='sgd', metrics=['accuracy'])
+yValidPredAlex = alexmodel.predict_generator(validDataGenerator2D(),val_samples=len(validationIDs))
+
+
+ts = time.time()
+st = datetime.datetime.fromtimestamp(ts).strftime('%Y_%m_%d__%H_%M_%S')
+fileName = 'cnnPredictions/cnnPredictionAlexNetFrom_'+st+'.mat'
+
+#sio.savemat(fileName,mdict={'yValidProb':yValidProb,'yValidPred':yValidPred,'yValidClasses':yValidClasses})
+sio.savemat(fileName,mdict={'yValidPredAlex':yValidPredAlex})
+
+
+"""
+sio.savemat('RES_randomProj.mat',mdict={'yHatTrainP':yHatTrainP,'yHatTestP':yHatTestP,'YvalidP':YvalidP,'Ytrain':Ytrain,'Ytest':Ytest})
+
+Yvalid = YvalidP[:,1]
+
+with open('submissionRandProjRandomForest.csv', 'w') as csvfile:
+    fieldnames = ['id', 'cancer']
+    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
+    writer.writeheader()
+    for ind in range(numValid):
+        writer.writerow({'id': validationIDs[ind], 'cancer': str(Yvalid[ind])})
+
+"""
